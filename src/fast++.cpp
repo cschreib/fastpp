ee0bfbc5bce2aa20a2a396bc010cc941d20c9d13
cschreib <cschreib@orange.fr>
2017-06-14 11:15:48 +0200

    Set version number to 1.0

--- src/fast++.cpp
+++ src/fast++.cpp
@@ -1,7 +1,7 @@
 #include "fast++.hpp"
 #include <phypp/core/main.hpp>
 
-const char* fastpp_version = "0.1";
+const char* fastpp_version = "legacy-1.0";
 
 int phypp_main(int argc, char* argv[]) {
     std::string param_file = (argc >= 2 ? argv[1] : "fast.param");
