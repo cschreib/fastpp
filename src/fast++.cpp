3e38a0805c6dbd32ae7566fe3eb0d74d1bcfb1b3
cschreib <cschreib@orange.fr>
2017-10-20 23:22:44 +0200

    Added definitions for static const constexpr.

--- src/fast++.cpp
+++ src/fast++.cpp
@@ -3,6 +3,22 @@
 
 const char* fastpp_version = "1.0";
 
+const constexpr uint_t grid_id::z;
+const constexpr uint_t grid_id::av;
+const constexpr uint_t grid_id::age;
+const constexpr uint_t grid_id::metal;
+const constexpr uint_t grid_id::custom;
+
+const constexpr uint_t prop_id::scale;
+const constexpr uint_t prop_id::spec_scale;
+const constexpr uint_t prop_id::mass;
+const constexpr uint_t prop_id::sfr;
+const constexpr uint_t prop_id::ssfr;
+const constexpr uint_t prop_id::ldust;
+const constexpr uint_t prop_id::lion;
+const constexpr uint_t prop_id::mform;
+const constexpr uint_t prop_id::custom;
+
 int phypp_main(int argc, char* argv[]) {
     std::string param_file = (argc >= 2 ? argv[1] : "fast.param");
 
